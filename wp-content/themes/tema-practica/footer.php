9b4bdfbcd243f695fc5adfe8171f830f18471d40
chehin <chehinjuan@gmail.com>
2021-11-28 19:23:54 -0300

    nav tags en 'tema-practica'

--- wp-content/themes/tema-practica/footer.php
+++ wp-content/themes/tema-practica/footer.php
@@ -1,3 +1,29 @@
 <?php bloginfo(); ?>
 
+<?php
+$array = array(
+	'theme_location'  => 'tema-practica',
+	'menu'            => '',
+	'container'       => 'div',
+	'container_class' => '',
+	'container_id'    => '',
+	'menu_class'      => 'menu',
+	'menu_id'         => '',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '',
+    'item_spacing'    => 'preserve',
+	'depth'           => 3,
+	'walker'          => ''
+);
+ 
+wp_nav_menu( $array );
+
+?>
+ <!-- wp_nav_menu('items_wrap')  -->
+
 <?php wp_footer(); ?>
